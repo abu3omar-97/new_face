fb9ca49f0fb74b3bbae3560f748b4fb2827e62fc
Yaman <yalashqar97@gmail.com>
2020-08-21 14:33:32 +0300

    Sign In page (login) NOTE: I added all the widgets inside a listview so that the keyboard does not cover the text fields ... we could fix it later after testing

--- lib/src/pages/sign_in_page.dart
+++ lib/src/pages/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -5,24 +6,137 @@ class SignInPage extends StatefulWidget {
   _SignInPageState createState() => _SignInPageState();
 }
 
+TextStyle textFieldStyle = TextStyle(fontSize: 20, color: Colors.blueGrey);
+
 class _SignInPageState extends State<SignInPage> {
+  bool _toogleVisibilty = true;
+  IconData visible = Icons.visibility_off;
+
+  Widget _buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          hintText: "Enter Your Email ...", hintStyle: textFieldStyle),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                _toogleVisibilty = !_toogleVisibilty;
+                if (!_toogleVisibilty)
+                  visible = Icons.visibility;
+                else
+                  visible = Icons.visibility_off;
+              });
+            },
+            icon: Icon(visible),
+          ),
+          hintText: "Enter Your Password ...",
+          hintStyle: textFieldStyle),
+      obscureText: _toogleVisibilty,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: <Widget>[
-            Text(
-              "Sign In",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
+        // resizeToAvoidBottomPadding: false,
+        body: ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 100),
+//            Row(
+//              mainAxisAlignment: MainAxisAlignment.center,
+//              children: <Widget>[
+//                Icon(Icons.restaurant, size: 50),
+//                SizedBox(width: 10),
+//                Icon(Icons.fastfood, size: 50),
+//              ],
+//            ),
+              Text(
+                "Sign In",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontSize: 50,
+                    fontFamily: 'Pac'),
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              Card(
+                elevation: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      _buildEmailTextField(),
+                      SizedBox(height: 10),
+                      _buildPasswordTextField(),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                margin: EdgeInsets.only(left: 5, right: 5),
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(30)),
+                child: Center(
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Divider(
+                height: 30,
+                color: Colors.black87,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Don't have an account yet? | ",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  Text(
+                    "Sign Up Now",
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      ],
+    ));
   }
 }
